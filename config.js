e2420501e36b208f98a7188763519fa149a8b4d8
nguyễn minh đạt <71458580+datrongvang@users.noreply.github.com>
2022-03-08 10:00:52 +0700

    Update config.js
    ko thuong e

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
-const nameGirl = 'Em';
+const nameGirl = '';
 const giftUrl = 'http://nodemy.vn';
 const eventName = 'Chúc Mừng 8/3 của Nhi';
-const titleCard = 'Tặng người ấy';
-const contentCard = 'Chúc Nhi một ngày 8/3 thật may mắn và xinh đẹp! Gặp được e là điều may mắn a nhận được trong năm nay. Cầu chúc những điều tốt đẹp luôn bên e. Thương e!!';
+const titleCard = 'Tặng Nhi';
+const contentCard = 'Chúc Nhi một ngày 8/3 thật may mắn và xinh đẹp! Gặp được e là điều may mắn a nhận được trong năm nay. Cầu chúc những điều tốt đẹp luôn bên e. KO Thương e nữa!!';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
-// Mr-Nam http://facebook.com/nam.nodemy
+
 // Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
 const giftImage = 'hot-girl.png';
 const base64 = '';
