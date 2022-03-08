15599a45b8828ef9c2df5a0959c565decdff5167
nguyễn minh đạt <71458580+datrongvang@users.noreply.github.com>
2022-03-08 10:07:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const nameGirl = '';
-const giftUrl = 'http://nodemy.vn';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100007744054576';
 const eventName = 'Chúc Mừng 8/3 của Nhi';
 const titleCard = 'Tặng Nhi';
-const contentCard = 'Chúc Nhi một ngày 8/3 thật may mắn và xinh đẹp! Gặp được e là điều may mắn a nhận được trong năm nay. Cầu chúc những điều tốt đẹp luôn bên e. KO Thương e nữa!!';
+const contentCard = 'Chúc Nhi một ngày 8/3 thật may mắn và xinh đẹp! Gặp được e là điều may mắn a nhận được trong năm nay. Cầu chúc những điều tốt đẹp luôn bên e. Ko Thương e nữa!!';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
